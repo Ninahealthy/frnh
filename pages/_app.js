976514d7f5b4fc0be6ee52a04d51bbb18de9c017
Ninahealthy <olij2637@gmail.com>
2023-04-06 03:18:58 +0000

    060420230418

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ import "@/styles/globals.css";
 import Head from "next/head";
 import Actualites from "@/components/Actualites";
 import Consent from "@/components/Consent";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -23,6 +24,7 @@ export default function App({ Component, pageProps }) {
       <Author />
       <Footer />
       <Consent />
+      <Analytics />
     </>
   );
 }
