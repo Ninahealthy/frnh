748a65f5c15bfe17d2a5a0acbba84e59393f46b0
Ninahealthy <olij2637@gmail.com>
2023-04-18 00:08:29 +0000

    180420231208

---
+++ .next/server/chunks/543.js
@@ -0,0 +1,101 @@
+"use strict";
+exports.id = 543;
+exports.ids = [543];
+exports.modules = {
+
+/***/ 5543:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ Fitness)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: ./styles/Card.module.css
+var Card_module = __webpack_require__(1445);
+var Card_module_default = /*#__PURE__*/__webpack_require__.n(Card_module);
+// EXTERNAL MODULE: ./node_modules/next/image.js
+var next_image = __webpack_require__(5675);
+var image_default = /*#__PURE__*/__webpack_require__.n(next_image);
+;// CONCATENATED MODULE: ./public/tracker-de-fitness.jpg
+/* harmony default export */ const tracker_de_fitness = ({"src":"/_next/static/media/tracker-de-fitness.578b0457.jpg","height":1024,"width":1024,"blurDataURL":"data:image/jpeg;base64,/9j/2wBDAAoHBwgHBgoICAgLCgoLDhgQDg0NDh0VFhEYIx8lJCIfIiEmKzcvJik0KSEiMEExNDk7Pj4+JS5ESUM8SDc9Pjv/2wBDAQoLCw4NDhwQEBw7KCIoOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozv/wAARCAAIAAgDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAX/xAAeEAABAwQDAAAAAAAAAAAAAAABAAIDBBETMQUVgf/EABQBAQAAAAAAAAAAAAAAAAAAAAT/xAAZEQACAwEAAAAAAAAAAAAAAAABEQADUSL/2gAMAwEAAhEDEQA/AJlJNDxckDJqx3Zx4jGRSg4zYtABG7Dd9+IiIdoSOxVPTGT/2Q==","blurWidth":8,"blurHeight":8});
+;// CONCATENATED MODULE: ./public/running-shoes-women.jpg
+/* harmony default export */ const running_shoes_women = ({"src":"/_next/static/media/running-shoes-women.6bebc9b1.jpg","height":1024,"width":1024,"blurDataURL":"data:image/jpeg;base64,/9j/2wBDAAoHBwgHBgoICAgLCgoLDhgQDg0NDh0VFhEYIx8lJCIfIiEmKzcvJik0KSEiMEExNDk7Pj4+JS5ESUM8SDc9Pjv/2wBDAQoLCw4NDhwQEBw7KCIoOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozv/wAARCAAIAAgDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAL/xAAdEAACAgMAAwAAAAAAAAAAAAABAgAEAwUREiEi/8QAFAEBAAAAAAAAAAAAAAAAAAAABP/EABgRAAMBAQAAAAAAAAAAAAAAAAECEQAx/9oADAMBAAIRAxEAPwCqmu5dr5rugWxkNvOc5SwAro6hV4Se/Pj6B5ERBMiGVRmijh3/2Q==","blurWidth":8,"blurHeight":8});
+;// CONCATENATED MODULE: ./components/Fitness.js
+
+
+
+
+
+const responsive = {
+    width: "100%",
+    height: "auto"
+};
+function Fitness() {
+    return /*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("group", {
+            className: (Card_module_default()).group,
+            children: [
+                /*#__PURE__*/ jsx_runtime_.jsx("item", {
+                    className: (Card_module_default()).item,
+                    children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                        href: "/Fitness/meilleurs-trackers-de-fitness-pour-les-femmes",
+                        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("card", {
+                            className: (Card_module_default()).card,
+                            children: [
+                                /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                                    src: tracker_de_fitness,
+                                    alt: "terrasse ext\xe9rieur",
+                                    style: responsive,
+                                    priority: true,
+                                    sizes: "100vw"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                                    children: /*#__PURE__*/ jsx_runtime_.jsx("strong", {
+                                        children: /*#__PURE__*/ jsx_runtime_.jsx("em", {
+                                            children: "Les Meilleurs Trackers De Fitness Pour Les Femmes en 2023"
+                                        })
+                                    })
+                                })
+                            ]
+                        })
+                    })
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("item", {
+                    className: (Card_module_default()).item,
+                    children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                        href: "/Fitness/meilleures-chaussures-de-course-pour-le-confort-et-le-soutien",
+                        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("card", {
+                            className: (Card_module_default()).card,
+                            children: [
+                                /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                                    src: running_shoes_women,
+                                    alt: "chaussures de course",
+                                    style: responsive,
+                                    priority: true,
+                                    sizes: "100vw"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                                    children: /*#__PURE__*/ jsx_runtime_.jsx("strong", {
+                                        children: /*#__PURE__*/ jsx_runtime_.jsx("em", {
+                                            children: "5 Chaussures De Course Qui Allient Confort Et Stabilit\xe9 En 2023"
+                                        })
+                                    })
+                                })
+                            ]
+                        })
+                    })
+                })
+            ]
+        })
+    });
+}
+
+
+/***/ })
+
+};
+;
--- components/Fitness.js
+++ components/Fitness.js
@@ -2,6 +2,7 @@ import Styles from "@/styles/Card.module.css";
 import Image from "next/image";
 
 import tracker from "public/tracker-de-fitness.jpg";
+import running from "public/running-shoes-women.jpg";
 
 const responsive = { width: "100%", height: "auto" };
 
@@ -23,6 +24,27 @@ export default function Fitness() {
                 <strong>
                   <em>
                     Les Meilleurs Trackers De Fitness Pour Les Femmes en 2023
+                  </em>
+                </strong>
+              </p>
+            </card>
+          </a>
+        </item>
+        <item className={Styles.item}>
+          <a href="/Fitness/meilleures-chaussures-de-course-pour-le-confort-et-le-soutien">
+            <card className={Styles.card}>
+              <Image
+                src={running}
+                alt="chaussures de course"
+                style={responsive}
+                priority
+                sizes="100vw"
+              />
+              <p>
+                <strong>
+                  <em>
+                    5 Chaussures De Course Qui Allient Confort Et Stabilité En
+                    2023
                   </em>
                 </strong>
               </p>
