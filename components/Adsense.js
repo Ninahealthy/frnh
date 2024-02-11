319dab21e3d741fed75574de28fadb667374e84d
Ninahealthy <olij2637@gmail.com>
2024-02-11 01:38:44 +0100

    110220240138

--- components/Adsense.js
+++ components/Adsense.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useRef } from "react";
 import Script from "next/script";
 
-function GoogleAdPcItem({ adId }) {
+function GoogleAdPcItem() {
   const router = useRouter();
   const adsLoaded = useRef(false);
 
@@ -17,34 +17,34 @@ function GoogleAdPcItem({ adId }) {
     };
 
     if (router.query && !adsLoaded.current) {
-      setTimeout(loadAd, 0);
+      setTimeout(loadAd, 100);
     }
   }, [router.query]);
 
   return (
-    <Script
-      key={adId}
-      ref={adsLoaded}
-      id={adId}
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2087636695455778"
-      crossorigin="anonymous"
-    />
-
-    /*<div
-      className="googleAd-container flex items-center justify-center border rounded-md"
-      style={{ maxWidth: '282px', maxHeight: '282px' }}
-    >
-      <ins
-        key={adId}
-        ref={adsLoaded}
-        id={adId}
-        className="adsbygoogle h-full"
-        style={{ display: 'inline-block', width: '282px', height: '282px' }}
-        data-ad-client="ca-pub-1111111"
-        data-full-width-responsive="true"
-        data-ad-slot="11111111"
+    <>
+      <Script
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2087636695455778"
+        crossorigin="anonymous"
       />
-    </div>*/
+
+      {/*<div
+        className="googleAd-container flex items-center justify-center border rounded-md"
+        style={{ maxWidth: "282px", maxHeight: "282px" }}
+      >
+        <ins
+          key={adId}
+          ref={adsLoaded}
+          id={adId}
+          className="adsbygoogle h-full"
+          style={{ display: "inline-block", width: "282px", height: "282px" }}
+          data-ad-client="ca-pub-1111111"
+          data-full-width-responsive="true"
+          data-ad-slot="11111111"
+        />
+      </div>
+      */}
+    </>
   );
 }
 
