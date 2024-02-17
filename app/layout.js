e77bdff1139e82230bad2b748b83bce0657a4e1b
Ninahealthy <olij2637@gmail.com>
2024-02-17 12:07:40 +0100

    170220241207

--- app/layout.js
+++ app/layout.js
@@ -44,7 +44,6 @@ export default function RootLayout({ children }) {
       <html lang="fr">
         <head>
           <Script
-            strategy="beforeInteractive"
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2087636695455778"
             crossOrigin="anonymous"
           ></Script>
